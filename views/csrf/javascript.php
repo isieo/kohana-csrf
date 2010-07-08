11efef5ee34dcbaffb54ada31b5521b6ead91ec6
Calvin Tee <isieox+github@gmail.com>
2010-07-08 16:36:08 +0800

    expose getCSRFToken() js to everyone

--- views/csrf/javascript.php
+++ views/csrf/javascript.php
@@ -1,20 +1,18 @@
+function getCSRFToken(){
+    var token=null;
+    $.ajax({
+                    url: "<?=url::site('/csrf/generate');?>",
+                    global: false,
+                    dataType: "json",
+                    async:false,
+                    success: function(msg){
+                       token = msg.token;
+                    }
+                 }
+              );
+    return token;
+}
 $(function(){
-    function getCSRFToken(){
-        var token=null;
-        $.ajax({
-                        url: "<?=url::site('/csrf/generate');?>",
-                        global: false,
-                        dataType: "json",
-                        async:false,
-                        success: function(msg){
-                           token = msg.token;
-                        }
-                     }
-                  );
-        return token;
-    }
-
-
     $('form').submit(function(){
         theToken = getCSRFToken();
         $(this).append('<input type="hidden" name="csrf-token" value="' + theToken + '">');
